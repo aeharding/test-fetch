0597bd0ddabcea48cabd855d7d3b13621f604080
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-10-23 12:10:36 -0500

    Add repro

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,17 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'test-fetch',
-  webDir: 'dist',
+  appId: "io.ionic.starter",
+  appName: "test-fetch",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
